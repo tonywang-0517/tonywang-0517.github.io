e9bc92e8964396ece084daa96d30aba43d74ad85
Anthony Wang <anthonywang0517@gmail.com>
2022-10-14 21:24:58 +1300

    add nextjs action

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? "/tonywang-0517.github.io/" : "",
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
