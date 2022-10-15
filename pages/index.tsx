8014f617406aff3c47c0060fc8d48c91bc1424ca
Anthony Wang <anthonywang0517@gmail.com>
2022-10-15 23:13:26 +1300

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type {NextPage} from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
 import {Carousel} from "flowbite-react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {
+    faReact, faJs,faHtml5,faCss3,faSass,faBootstrap, faFigma, faNodeJs,faAws,faDocker,faGit,faTrello
+} from "@fortawesome/free-brands-svg-icons";
+
 
 const logos = [
     '/static/freightways.png',
@@ -40,24 +45,41 @@ const projects = [
     }
 ]
 
+const techCards = [
+    {
+        title:'Front-End',
+        items:[{icon:faReact,text:'React (NextJS, Gatsby, CRA)'},{icon:faJs,text:'JavaScript'},{icon:'',text:'TypeScript'},{icon:faHtml5,text:'HTML5'},{icon:faCss3,text:'CSS3'},{icon:faSass,text:'SASS'},{icon:faBootstrap,text:'Bootstrap 5, Styled Components'},{icon:faFigma,text:'UI Design (Figma)'}]
+    },
+    {
+        title:'Back-End',
+        items:[{icon:'',text:'GraphQL'},{icon:'',text:'SQL'},{icon:faNodeJs,text:'Node & Express'},{icon:faDocker,text:'Docker'},{icon:faAws,text:'AWS EC2, CloudFront, S3, Route53'},{icon:faNodeJs,text:'NodeJs'}]
+    },
+    {
+        title:'THE FUNDAMENTALS',
+        items:[{icon:'',text:'Data Structures & Algorithms'},{icon:'',text:'Test Driven Development (TDD)'},{icon:'',text:'Design Patterns'},{icon:faTrello,text:'Agile (Scrum, Kanban)'},{icon:faGit,text:'Git'},{icon:'',text:'Design Thinking'}]
+    },
+    {
+        title:'OTHER',
+        items:[{icon:'',text:'Process Mining'}]
+    }
+];
 
 const Home: NextPage = () => {
 
 
     return (
         <div>
-            <header className="text-gray-100 bg-lime-500 w-full z-50 py-2">
+            <header className="text-gray-100 bg-gradient-to-b from-slate-900 to-slate-600 w-full z-50 py-2">
 
                 <div className="lg:container w-full  flex flex-col md:flex-row  items-center justify-center md:justify-between  md:px-8 xl:px-0  py-8 md:py-8 lg:py-20 mx-auto">
                     <hgroup
                         className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl p-5 font-bold lg:w-3/5 flex flex-col items-center md:items-start">
-                  <span className="flex items-baseline"><h1
-                      className="text-white font-title">Tony Wang</h1></span><h2
-                        className="bg-clip-text text-white font-title">Senior Front-end Developer</h2>
-                        <p className={'text-lg'}>Bla Bla...</p>
+                        <h1 className="leading-tight text-white">Tony Wang</h1>
+                        <h2 className="bg-clip-text text-transparent bg-gradient-to-r to-sky-400 from-violet-400">Senior Front-end Developer</h2>
+                        <p className={'mt-2 lg:mt-5 text-base sm:text-base lg:text-lg xl:text-lg font-light text-white'}>Focused on accomplishing challenging development goals under strict deadlines. Analytical and resourceful problem-solver with disciplined approach and skill in producing clean, robust code.</p>
                     </hgroup>
                     <picture className="flex justify-center items-center w-1/2 sm:w-2/5">
-                        <Image width="756" height="512"  src="/static/svg-hero-art-538ee183e050d77af16d88c5cec3b80d.svg" className="md:max-h-32 lg:max-h-80" alt="artwork"/></picture>
+                        <Image width="268" height="274"  src="/static/portrait.png" className="md:max-h-32 lg:max-h-80" alt="artwork"/></picture>
                 </div>
             </header>
             <main>
@@ -73,7 +95,7 @@ const Home: NextPage = () => {
                             {
                                 logos.map((logo,key)=>(
                                     <a key={key}>
-                                        <div  className={'w-32 h-32  flex rounded-full bg-stone-900 place-items-center'}>
+                                        <div  className={'w-32 h-32  flex rounded-full bg-slate-900 place-items-center'}>
                                             <div className={'w-5/6 h-1/2 relative fill-white m-auto'}>
                                                 <Image  layout='fill' className={'fill-white text-white'}  src={logo} objectFit="contain"/>
                                             </div>
@@ -97,21 +119,21 @@ const Home: NextPage = () => {
                         {
                             projects.map((project,index)=>(
                                 <div key={index} className="flex flex-col py-10 w-full h-2/6" >
-                                    <div className={`rounded-lg shadow-lg hover:shadow-xl bg-white text-black flex flex-col overflow-hidden ${index%2?'lg:flex-row':'lg:flex-row-reverse'}`} >
+                                    <div className={`md:rounded-lg shadow-lg hover:shadow-xl bg-white text-black flex flex-col overflow-hidden ${index%2?'lg:flex-row':'lg:flex-row-reverse'}`} >
                                         <div className='basis-full md:basis-3/5'>
                                             {
                                                 project.srcs.length !== 1?<Carousel slideInterval={20*1000} >
                                                     {
                                                         project.srcs.map((src,key)=>(
-                                                            <div key={key} className={'relative overflow-hidden'} style={{height:'614px'}}>
+                                                            <div key={key} className={'relative overflow-hidden'} >
                                                                 <div className={'absolute w-full h-full bg-stone-900 z-10 opacity-20'}></div>
-                                                                <Image src={src}  layout={'fill'}  objectFit={'cover'} objectPosition={'top'}/>
+                                                                <Image src={src}  width={1242} height={1242} layout={'responsive'}  objectFit={'cover'} objectPosition={'top'}/>
                                                             </div>
                                                         ))
                                                     }
-                                                </Carousel>:<div className={'relative overflow-hidden'} style={{height:'614px'}}>
+                                                </Carousel>:<div className={'relative overflow-hidden'} >
                                                     <div className={'absolute w-full h-full bg-stone-900 z-10 opacity-20'}></div>
-                                                    <Image src={project.srcs[0]}  layout={'fill'}  objectFit={'cover'} objectPosition={'top'}/>
+                                                    <Image src={project.srcs[0]}  width={1242} height={1242} layout={'responsive'}  objectFit={'cover'} objectPosition={'top'}/>
                                                 </div>
                                             }
 
@@ -147,55 +169,25 @@ const Home: NextPage = () => {
                                 Technologies & Skills</p>
                         </div>
                         <div className={'grid grid-cols-1 md:grid-cols-2 gap-5 w-full my-10'}>
-                            <div className={'rounded-lg shadow-lg hover:shadow-xl bg-white text-black p-8'}>
-                                <p className={'uppercase mb-5 font-bold tracking-widest'}>Front-end</p>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                            </div>
-                            <div className={'rounded-lg shadow-lg hover:shadow-xl bg-white text-black p-8'}>
-                                <p className={'uppercase mb-5 font-bold tracking-widest'}>Front-end</p>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                            </div>
-                            <div className={'rounded-lg shadow-lg hover:shadow-xl bg-white text-black p-8'}>
-                                <p className={'uppercase mb-5 font-bold tracking-widest'}>Front-end</p>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                            </div>
-                            <div className={'rounded-lg shadow-lg hover:shadow-xl bg-white text-black p-8'}>
-                                <p className={'uppercase mb-5 font-bold tracking-widest'}>Front-end</p>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                                <div className={'flex flex-row'}>
-                                    <p className={'basis-16'}>icon</p><p className={'basis-auto'}>React</p>
-                                </div>
-                            </div>
+                            {
+                                techCards.map((techcard,index)=>(
+                                    <div key={index} className={'rounded-lg shadow-lg hover:shadow-xl bg-white text-black p-8'}>
+                                        <p className={'uppercase mb-5 font-bold tracking-widest'}>{techcard.title}</p>
+                                        {
+                                            techcard.items.map((item,index)=>(
+                                                <div key={index} className={'flex flex-row'}>
+                                                    <p className={'basis-6'}>
+                                                        <FontAwesomeIcon icon={item.icon}/>
+                                                    </p>
+                                                    <p className={'basis-auto'}>{item.text}</p>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                ))
+                            }
                         </div>
+
                         <div className="flex flex-col items-center ">
                             <p className="font-title text-2xl sm:text-3xl font-extrabold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-700 text-center">
                                 Recommendations</p>
