74db9652c6cb072978eb8741e60787eb6144fe1b
Anthony Wang <anthonywang0517@gmail.com>
2022-10-17 00:22:17 +1300

    fix issue https://github.com/vercel/next.js/issues/20682

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
-import '../styles/globals.scss'
-import type {AppProps} from 'next/app'
+import '../styles/globals.scss';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import type {AppProps} from 'next/app';
 import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
     return <>
         <Head>
             <title>Tony Wang - Resume</title>
-            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
         </Head>
         <Component {...pageProps} />
     </>
